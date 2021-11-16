01a96f85bcef3adc4a609f9f135d5eae5d28682f
Daniel M <dmolinac@gmail.com>
2021-11-16 09:07:22 +0100

    Updates

---
+++ precache-manifest.528ea3f037150e2ea343f81f85a64537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3abb83b2689374b81efdfd34705fcf",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "6439bda54a35daf97061",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.e79a779b.chunk.css"
+  },
+  {
+    "revision": "2e976ccfe72d5af466cd",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.3d2211c9.chunk.js"
+  },
+  {
+    "revision": "6439bda54a35daf97061",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.e9caf7da.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
