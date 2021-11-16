f748f44a1bdaf4cd085f1f06f5a27f4797d63c3e
Daniel M <dmolinac@gmail.com>
2021-11-16 12:04:12 +0100

    First version, previously ignored by Git

---
+++ truffle-config.js
@@ -0,0 +1,41 @@
+require("dotenv").config
+const path = require("path");
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    kovan: {
+      networkCheckTimeout: 100000,
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}");
+      },
+      network_id: 42
+    }
+  },
+// Configure your compilers
+  compilers: {
+    solc: {
+      version: "pragma",       // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  },
+  development: {
+    port: 8545,
+    network_id: 42,
+    accounts: 5,
+    defaultEtherBalance: 500,
+    blockTime: 3
+  }
+};
